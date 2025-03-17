fcee79b8a066efd32510d1eda8ade14cdc53b07d
shavi-shavi <irushi.20220249@iit.ac.lk>
2025-03-17 19:00:35 +0530

    inputs

--- Api requestt wd.py
+++ Api requestt wd.py
@@ -1,3 +1,11 @@
 import requests
 # Define the API URL
 url = 'http://127.0.0.1:5000/predict'
+
+# Define the input data
+data = {
+    'dist': 100.0,
+    'x_nad83': 500000.0,
+    'y_nad83': 4500000.0,
+    'elev': 50.0
+}
