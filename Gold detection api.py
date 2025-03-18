88d754bd2e3295fa6fb5d58de6de7c508dc522b6
shavi-shavi <irushi.20220249@iit.ac.lk>
2025-03-18 09:46:24 +0530

    database

--- Gold detection api.py
+++ Gold detection api.py
@@ -7,3 +7,7 @@ from sklearn.preprocessing import MinMaxScaler
 
 app = Flask(__name__)
 
+# Database configuration (SQLite for simplicity)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///gold_detection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
