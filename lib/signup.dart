ff80ca55b61be6eff45b2730124eff283c221534
Dulmi Dissanayake <disdul11@gmail.com>
2025-03-22 17:52:29 +0530

    Added new background image for the signup page

--- lib/signup.dart
+++ lib/signup.dart
@@ -44,18 +44,13 @@ class _SignUpPageState extends State<SignUpPage> {
     return Scaffold(
       body: Stack(
         children: [
-          // Background gradient
+          // Background Image
           Container(
             decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  const Color.fromARGB(255, 250, 219, 230),
-                  const Color.fromARGB(255, 243, 182, 216),
-                  const Color.fromARGB(255, 179, 123, 154),
-                  const Color.fromARGB(255, 146, 73, 114),
-                ],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
+              image: DecorationImage(
+                image: AssetImage('assets/gem_bg_new.png'),
+                fit: BoxFit.cover,
+                alignment: Alignment.center,
               ),
             ),
           ),
@@ -99,7 +94,7 @@ class _SignUpPageState extends State<SignUpPage> {
                       _buildTextField('Mobile', _mobileController, (value) {
                         if (value == null || value.isEmpty) {
                           return 'Please enter your mobile number';
-                        }else if (!RegExp(r'^\d{10}$').hasMatch(value)) {
+                        } else if (!RegExp(r'^\d{10}$').hasMatch(value)) {
                           return 'Please enter a valid mobile number';
                         }
                         return null;
